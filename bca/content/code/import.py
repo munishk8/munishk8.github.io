b8375edf1c9f2ac339fcbc1bae1c086df744b3c3
Munish K <mk6229478@gmail.com>
2021-12-27 20:14:20 +0530

    update

--- bca/content/code/import.py
+++
@@ -1,9 +0,0 @@
-# Program to demonstrate the statement: from ... import
-
-from math import sqrt       # use from ... import
-
-print(sqrt(4))              # Use imported function
-
-
-
-
--- bca/content/code/lambdaMap.py
+++
@@ -1,9 +0,0 @@
-# Program using lambda function: map()
-
-ls = [1,2,3,4,5]                            # Taken a list
-
-newList = list(map(lambda x:(x*x), ls))     # Square each number
-print(newList)                              # Print result
-
-
-
